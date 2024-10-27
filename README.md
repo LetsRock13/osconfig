35d7b208e6585f97cec6e1e111c4aa0cbf5d589b
LetsRock13 <r.hingst@web.de>
2024-10-28 00:13:06 +0100

    Added some basic stugg

---
+++ README.md
@@ -0,0 +1,1 @@
+This is just a little script to make a new os setup easier
--- osconfig.sh
+++ osconfig.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 CMD=$1
+SCRIPT="osconfig"
+INSTPROGS="git nvim gcc"
 
 help() {
 
-	echo -en ""
-	echo -en ""
-	echo -en ""
-	echo -en ""
+	echo -en "\tUsage\n"
+	echo -en "\t$SCRIPT <option>\n"
+	echo -en "\tOptions\n"
+	echo -en "\t\t<-h> 			Calls this Help\n"
+	echo -en "\t\t<-all> 		Call the complite Script\n"
+	echo -en "\t\t<-upadte> 	Calls Update\n"
+	echo -en "\t\t<-install> 	Calls Install\n"
+}
 
+update() {
+	sudo apt update && sudo apt dist-upgrade
+}
+
+install() {
+	sudo apt install $INSTPROGS
 }
 
 main() {
@@ -16,6 +28,14 @@ main() {
 	case $CMD in
 		"-h")
 			help;;
+		"-all")
+			update
+			install
+			;;
+		"-update")
+			update;;
+		"-install")
+			install;;
 		*)
 			help;;
 	esac
